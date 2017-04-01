7a636f1a6912dfa05d9cd4bb4181eaa1238119da
David Simmen <david.simmen@bluewin.ch>
2017-04-01 20:37:29 +0200

    added include to urls

--- demo/urls.py
+++ demo/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 import juntagrico
 
 urlpatterns = [
-    url(r'^', juntagrico.urls.urlpatterns),
+    url(r'^', include('juntagrico.urls.urlpatterns')),
     url(r'^', juntagrico.views.home),
     url(r'^impersonate/', include('impersonate.urls')),
 ]
