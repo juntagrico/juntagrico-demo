5e246a302de47ab41b9b929b4b283ac12e0379db
pascalfree <pascal.free@ihdg.ch>
2026-03-31 02:56:50 +0200

    staging

--- demo/settings.py
+++ demo/settings.py
@@ -228,3 +228,10 @@ TEST_MODE = os.environ.get("JUNTAGRICO_TEST_MODE", 'False')=='True'
 
 if TEST_MODE:
     INSTALLED_APPS.append('testmode')
+
+
+# Staging
+if os.environ.get('JUNTAGRICO_STAGING') == '1':
+    # staging URL erlauben
+    if not DEBUG:
+        ALLOWED_HOSTS.append('demo-staging.juntagrico.science')
